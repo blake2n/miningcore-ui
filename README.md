1df746cd2460cb534fe0e871dbb4c0dd22d0640a
BLAKE2Share <dev@easymine.rocks>
2019-02-02 16:02:36 -0500

    Aionmine Skin
    Aionmine Skin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# miningcore-ui has moved to Gitlab.
+# Aionmine.org Template
+# Aionmine is always original <-> fair <-> made for the community
+# this mine was not made because we are hedging on aion <-> it was made to support aion
+# Thank you for the work CalvinTam236
 ## https://gitlab.com/calvintam236/miningcore-ui
---
+++ assets/configs/mine_aion.bat
@@ -0,0 +1,2 @@
+miner.exe --algo 210_9 --pers AION0PoW --server stratum.aionmine.org --port 3333 --user 0xa07978089c76e2010720bec8400f05530cd946a567dc0fd9b862d83a45681f7c.rigName1 --pass x
+ --server stratum.glorypool.io --port 3333 --user 0xa07978089c76e2010720bec8400f05530cd946a567dc0fd9b862d83a45681f7c.rigName1 --pass x
